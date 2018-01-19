2f607b074dae90a7bbd6fb0b56ba59c0a5e1b4be
Kenneth Fowler <kenfowler@comcast.net>
2018-01-19 18:46:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function sayHiToGrandma(string) {
   } else if ( string.toUpperCase() === string) {
     console.log("YES INDEED!")
   } else {
-    console.log("I can't hear you!")
+    console.log("I can\'t hear you!")
   }
 }
