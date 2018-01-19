a6f79936e51974ae3a58821f60f74283556801a3
Kenneth Fowler <kenfowler@comcast.net>
2018-01-19 18:49:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if ( string === 'I love you, Grandma' ) {
+  if ( string === 'I love you, Grandma.' ) {
 //    console.log("I love you, too.")
       return 'I love you, too.'
   } else if ( string.toUpperCase() === string) {
